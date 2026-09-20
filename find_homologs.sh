85b9a5f271791023086aeed6bfe5338165655b99
sgupta820 <sgupta820@gatech.edu>
2026-09-19 20:32:38 -0400

    Fix the output directory handling because autograder does not like

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,7 +4,8 @@ query=$1
 subject=$2
 output=$3
 
-tblastn -query "$query" -subject "$subject" -outfmt "6 qseqid sseqid pident length qlen qstart qend sstart send evalue bitscore" | awk '$3 > 30'
+mkdir -p "$(dirname "$output")"
+
+tblastn -query "$query" -subject "$subject" -outfmt "6 qseqid sseqid pident length qlen qstart qend sstart send evalue bitscore" | awk '$3 > 30 && $4 > (0.9 * $5)' > "$output"
 
 wc -l < "$output"
-
